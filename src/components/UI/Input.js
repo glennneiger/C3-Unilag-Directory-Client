5c12fd8f6b75c9a5f76a0216a10b57a0d82cb191
olorondu_emeka <oloronduchukwuemeka@gmail.com>
2018-12-03 16:39:06 +0100

    made year of graduation dropdown dependent on current year

---
+++ src/components/UI/Input.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import Wrapper from '../../hoc/Wrapper';
+
+const input = (props) => {
+    let inputElement = '';
+
+    switch(props['input_type']){
+        case 'input':
+            inputElement =
+                <input
+                    type={props.theConfig.type}
+                    value={props.theValue}
+                    onChange={props.changed}
+                    required
+                />;
+            break;
+
+        case 'select':
+            inputElement =
+                <select
+                    className="custom-select"
+                    value={props.theValue}
+                    onChange={props.changed}
+                    required>
+                        {props.theConfig.optionValues.map(optionValue => (
+                            <option key={optionValue.value} value={optionValue.value}>{optionValue.displayValue}</option>
+                        ))
+                        }
+                </select> ;
+            break;
+
+        default:
+            inputElement = <input />
+
+    }
+
+
+
+   return (
+        <Wrapper>
+            <label> {props.label} <span className="text-danger"> <b>*</b> </span> </label>
+            {inputElement}
+        </Wrapper>
+   );
+};
+
+export default input;
--- src/containers/auth/School_Admin_Register.js
+++ src/containers/auth/School_Admin_Register.js
@@ -1,14 +1,155 @@
 import React, { Component } from 'react';
 
 import HomeHeader from '../../components/UI/Home_Header';
+import Input from '../../components/UI/Input';
 
 class School_Admin_Register extends Component{
-    handleChange = (event) => {
-        console.log(event.target.value);
+    currentYear = new Date().getFullYear();
+    yearArray = [];
+
+    state = {
+        registerForm: {
+           surname: {
+               elementType: 'input',
+               elementConfig: {
+                   type: 'text'
+               },
+               value: '',
+               label: 'Surname'
+           },
+            firstName: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text'
+                },
+                value: '',
+                label: 'First Name'
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email'
+                },
+                value: '',
+                label: 'Email'
+            },
+            password: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'password'
+                },
+                value: '',
+                label: 'Password'
+            },
+            passwordConfirm: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'password'
+                },
+                value: '',
+                label: 'Confirm Password'
+            },
+            service: {
+               elementType: 'select',
+                elementConfig: {
+                   defaultValue: '',
+                   optionValues: [
+                       { value: '', displayValue: 'Select a Service'},
+                       { value: 'first', displayValue: 'First Service'},
+                       { value: 'second', displayValue: 'Second Service'},
+                       { value: 'fourth', displayValue: 'Fourth Service'}
+                   ]
+                },
+                label: 'Service',
+                value: ''
+            },
+            gradYear: {
+                elementType: 'select',
+                elementConfig: {
+                    defaultValue: '',
+                    optionValues: [
+                        { value: '', displayValue: 'Select a Year' }
+                    ]
+                },
+                label: 'Expected Year of Graduation',
+                value: ''
+            }
+
+        }
+    };
+
+
+    //  make a shallow copy of registerForm in state
+    registerClone = {...this.state.registerForm};
+    gradYearClone = {...this.registerClone['gradYear']};
+    elementConfigClone = { ...this.gradYearClone.elementConfig };
+    optionsClone = [...this.elementConfigClone['optionValues']];
+
+    setYearArray = () => {
+        for(var i = 0; i <= 4; i++){
+            this.yearArray.push(this.currentYear + i);
+        }
+
+        var theMainArray = this.yearArray.map(element => {
+            return { value: element, displayValue: element }
+        });
+
+        theMainArray.forEach(item => {
+            this.optionsClone.push(item);
+        });
+
+        //fit back the clone pieces together
+        this.elementConfigClone['optionValues'] = this.optionsClone;
+        this.gradYearClone.elementConfig = this.elementConfigClone;
+        this.registerClone.gradYear = this.gradYearClone;
+
+        // update the state
+         this.setState({ registerForm: this.registerClone });
+
+    };
+
+    componentDidMount(){
+        this.setYearArray();
+    }
+
+
+    inputChangedHandler = (event, inputIdentifier) => {
+        const registerFormClone = { ...this.state.registerForm };
+        const registerFormKey_Value = { ...registerFormClone[inputIdentifier] };
+
+        registerFormKey_Value.value = event.target.value;
+        registerFormClone[inputIdentifier] = registerFormKey_Value;
+
+        //set the state on change
+        this.setState({ registerForm: registerFormClone });
+
+
     };
 
 
     render() {
+        let formElementsArray = [];
+        // create form elements array from state
+        for(let key in this.state.registerForm){
+           formElementsArray.push({
+               id: key,
+               config: this.state.registerForm[key]
+           });
+        }
+
+        let finalFormElements = formElementsArray.map(formElement => {
+            return (
+                <Input
+                    key={formElement.id}
+                    theConfig={formElement.config.elementConfig}
+                    label={formElement.config.label}
+                    input_type={formElement.config.elementType}
+                    theValue={formElement.config.value}
+                    changed={(event) => this.inputChangedHandler(event, formElement.id)}
+                />
+            );
+        });
+        
         return (
             <section>
                 <HomeHeader/>
@@ -16,48 +157,9 @@ class School_Admin_Register extends Component{
                     <div className="form-header">
                         <h1 style={{ marginBottom: '0px' }}>School Admin Registration</h1>
                     </div>
-                    <p style={{ padding: '5px 20px 0px', marginBottom: '0px'}}> <span className="text-danger">* </span> field is required</p>
+                    <p style={{ padding: '5px 20px 0px', marginBottom: '0px'}}> <span className="text-danger"> <b>*</b> </span> field is required</p>
                     <form onSubmit={this.submitForm}>
-                        <label>Surname <span className="text-danger">*</span> </label>
-                        <input type="text" name="surname" onChange={this.handleChange} required/>
-
-                        <div className="form-group">
-                            <label>First name <span className="text-danger">*</span> </label>
-                            <input className="form-control" type="text" name="firstName" onChange={this.handleChange} required/>
-                        </div>
-                        
-
-                        <label>Email <span className="text-danger">*</span> </label>
-                        <input type="email" name="email" onChange={this.handleChange} required/>
-                        
-                        <label>Password <span className="text-danger">*</span> </label>
-                        <input type="password" name="password" onChange={this.handleChange} required/>
-
-                        <label>Confirm Password <span className="text-danger">*</span> </label>
-                        <input type="password" name="password-confirm" onChange={this.handleChange} required/>
-
-                        <div className="form-group">
-                            <label>Service <span className="text-danger">*</span> </label>
-                            <select className="custom-select" onChange={this.handleChange} defaultValue="">
-                                <option value="first">First Service</option>
-                                <option value="second">Second Service</option>
-                                <option value="fourth">Fourth Service</option>
-                            </select>
-                        </div>
-
-
-                        <label>Expected Year of Graduation <span className="text-danger">*</span> </label>
-                        <select className="custom-select" onChange={this.handleChange} defaultValue="">
-
-                            <option value={2019}>2019</option>
-                            <option value={2020}>2020</option>
-                            <option value={2021}>2021</option>
-                            <option value={2022}>2022</option>
-                            <option value={2023}>2023</option>
-                            <option value={2024}>2024</option>
-                            <option value={2025}>2025</option>
-                        </select>
-
+                        {finalFormElements}
                         <input type="submit" value="submit" />
                     </form>
 
